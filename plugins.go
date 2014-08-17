ad8ece3b04e95e3e248b302e4e6944d271f2d95a
kyuri <karpenko_yuri@rambler.ru>
2014-08-17 22:51:48 +0400

    lint corrections

--- plugins.go
+++ plugins.go
@@ -1,20 +1,19 @@
 /*
-	Package plugins provides support for creating plugin-based applications.
+Package plugins provides support for creating plugin-based applications.
 
-	Following workflow is supposed:
-		1) main application creates RPC Host service;
-		2) plugin starts and connects to the RPC Host:
-			2.1) plugin informs RPC Host about service, provided by that plugin and requests port to serve on;
-	  		2.2) RPC Host checks if such service is not provided by other connected plugin and generates port for plugin to serve on;
-			2.3) plugin starts RPC server on port, provided by RPC Host, and informs RPC Host about it;
-			2.4) RPC Host registers plugin service;
-		3) when plugin terminates, it informs RPC Host; RPC Host unregisters plugin service;
-		4) when RPC Host terminates, it informs all connected plugins; plugins terminates;
-		5) when main application need to invoke some service method, RPC Host is used for dispatch it;
+Following workflow is supposed:
+	1) main application creates RPC Host service;
+	2) plugin starts and connects to the RPC Host:
+		2.1) plugin informs RPC Host about service, provided by that plugin and requests port to serve on;
+  		2.2) RPC Host checks if such service is not provided by other connected plugin and generates port for plugin to serve on;
+		2.3) plugin starts RPC server on port, provided by RPC Host, and informs RPC Host about it;
+		2.4) RPC Host registers plugin service;
+	3) when plugin terminates, it informs RPC Host; RPC Host unregisters plugin service;
+	4) when RPC Host terminates, it informs all connected plugins; plugins terminates;
+	5) when main application need to invoke some service method, RPC Host is used for dispatch it;
 
-	For handling remote calls net/rpc package usage is supposed.
-	A plugin must register object(s) that will be used for handling remote calls.
-
+For handling remote calls net/rpc package usage is supposed.
+A plugin must register object(s) that will be used for handling remote calls.
 */
 package plugins
 
@@ -124,9 +123,9 @@ func (s *rpcSrv) handleStop() {
 	os.Exit(0);
 }
 
-// newRpcServer creates a new rpcServer instance.
-func newRpcServer(opt *Options, rpcServices ...interface{}) (rpcServer, error) {
-	err := registerRpcServices(rpcServices...)
+// newRPCServer creates a new rpcServer instance.
+func newRPCServer(opt *Options, rpcServices ...interface{}) (rpcServer, error) {
+	err := registerRPCServices(rpcServices...)
 	if err == nil {
 		opt.applyDefaults("RPC Server")
 		listener, err := net.Listen("tcp", opt.listenAddr())
@@ -151,7 +150,7 @@ func newRpcServer(opt *Options, rpcServices ...interface{}) (rpcServer, error) {
 	return nil, err;
 }
 
-func registerRpcServices(rpcServices ...interface{}) error {
+func registerRPCServices(rpcServices ...interface{}) error {
 	for _, rpcService := range rpcServices {
 		err := rpc.Register(rpcService)
 		if err != nil {
@@ -180,15 +179,15 @@ func (c *rpcClnt) Close() error {
 	return c.clnt.Close()
 }
 
-// newRpcClient creates a new rpcClient instance.
-func newRpcClient(name string, options ...*Options) (rpcClient, error) {
+// newRPCClient creates a new rpcClient instance.
+func newRPCClient(name string, options ...*Options) (rpcClient, error) {
 	opt := prepareOptions(fmt.Sprintf("plugin %s", name), options...)
-	if clnt, err := rpc.Dial("tcp", opt.dialAddr()); err != nil {
-		return nil, err;
-	} else {
-		clnt := &rpcClnt{clnt: clnt, log: opt.Log}
-		return clnt, nil
+	clnt, err := rpc.Dial("tcp", opt.dialAddr())
+	if err == nil {
+		return &rpcClnt{clnt: clnt, log: opt.Log}, nil
 	}
+	return nil, err;
+
 }
 
 // A Host is a manager that plugins connects to.
@@ -198,8 +197,11 @@ type Host interface {
 	Call(serviceName string, serviceMethod string, args interface{}, reply interface{}) error
 }
 
-var ErrServiceNotRegistered = errors.New("Service is not registered")
-var ErrServiceAlreadyRegistered = errors.New("Service is already registered")
+// ErrServiceNotRegistered returns when trying to access non-registered service
+var ErrServiceNotRegistered = errors.New("service is not registered")
+
+// ErrServiceAlreadyRegistered throws when trying to register already registered service
+var ErrServiceAlreadyRegistered = errors.New("service is already registered")
 
 type connectedPlugin struct {
 	info	*PluginInfo
@@ -241,7 +243,7 @@ func (h *host) getNextPort() int {
 		rslt, h.availPorts = h.availPorts[i], h.availPorts[:i]
 	} else {
 		rslt = h.nextPort
-		h.nextPort += 1
+		h.nextPort++
 	}
 	return rslt
 }
@@ -256,7 +258,7 @@ func (h *host) removePlugin(serviceName string) {
 	if i := h.indexOf(serviceName); i>=0 {
 		cp := h.plugins[i]
 		var dummy int
-		cp.Call("RpcPlugin.Terminate", dummy, &dummy)
+		cp.Call("RPCPlugin.Terminate", dummy, &dummy)
 		h.log.Printf("Plugin disconnected: \"%s\"", cp.info.Name)
 		h.availPorts = append(h.availPorts, cp.info.Port)
 		h.plugins = append(h.plugins[:i], h.plugins[i+1:]...)
@@ -286,24 +288,23 @@ func NewHost(name string, options ...*Options) (Host, error) {
 		nextPort: opt.Port+1,
 		availPorts: make([]int, 0),
 		plugins: make([]*connectedPlugin, 0)}
-	rpcHostService := &RpcHost{host: h}
-	if srv, err := newRpcServer(&opt, rpcHostService); err == nil {
+	srv, err := newRPCServer(&opt, &RPCHost{host: h})
+	if err == nil {
 		h.srv = srv
 		srv.onStop(h.onStop)
 		return h, nil
-	} else {
-		opt.Log.Println(err)
-		return nil, err
 	}
+	opt.Log.Println(err)
+	return nil, err
 }
 
-// Host RPC service. Handles plugins requests.
-type RpcHost struct {
+// RPCHost implements Host RPC service. Handles plugins requests.
+type RPCHost struct {
 	host *host
 }
 
-// Generates port for plugin to serve on.
-func (rh *RpcHost) GetPort(info *PluginInfo, port *int) error {
+// GetPort generates port for plugin to serve on.
+func (rh *RPCHost) GetPort(info *PluginInfo, port *int) error {
 	err := rh.mustBeDisconnected(info)
 	if err == nil {
 		*port = rh.host.getNextPort()
@@ -311,12 +312,12 @@ func (rh *RpcHost) GetPort(info *PluginInfo, port *int) error {
 	return err
 }
 
-// Handles plugin connection to host.
-func (rh *RpcHost) ConnectPlugin(info *PluginInfo, _ *int) error {
+// ConnectPlugin handles plugin connection to host.
+func (rh *RPCHost) ConnectPlugin(info *PluginInfo, _ *int) error {
 	err := rh.mustBeDisconnected(info)
 	if err == nil {
 		opt := prepareOptions(info.Name, &Options{Port: info.Port, Log: rh.host.log})
-		if clnt, err := newRpcClient(info.Name, &opt); err == nil {
+		if clnt, err := newRPCClient(info.Name, &opt); err == nil {
 			cp := &connectedPlugin{info: info, clnt: clnt}
 			rh.host.appendPlugin(cp)
 			rh.host.log.Printf("Plugin connected: \"%s\", handling \"%s\", serves at %s", cp.info.Name, cp.info.ServiceName, opt.dialAddr())
@@ -326,8 +327,8 @@ func (rh *RpcHost) ConnectPlugin(info *PluginInfo, _ *int) error {
 	return err
 }
 
-// Handles plugin disconnection from host.
-func (rh *RpcHost) DisconnectPlugin(info *PluginInfo, _ *int) error {
+// DisconnectPlugin handles plugin disconnection from host.
+func (rh *RPCHost) DisconnectPlugin(info *PluginInfo, _ *int) error {
 	err := rh.mustBeConnected(info)
 	if err == nil {
 		rh.host.removePlugin(info.ServiceName)
@@ -335,14 +336,14 @@ func (rh *RpcHost) DisconnectPlugin(info *PluginInfo, _ *int) error {
 	return err
 }
 
-func (rh *RpcHost) mustBeDisconnected(info *PluginInfo) error {
+func (rh *RPCHost) mustBeDisconnected(info *PluginInfo) error {
 	if rh.host.indexOf(info.ServiceName)>=0 {
 		return ErrServiceAlreadyRegistered
 	}
 	return nil
 }
 
-func (rh *RpcHost) mustBeConnected(info *PluginInfo) error {
+func (rh *RPCHost) mustBeConnected(info *PluginInfo) error {
 	if rh.host.indexOf(info.ServiceName)<0 {
 		return ErrServiceNotRegistered
 	}
@@ -367,14 +368,14 @@ func (p *plugin) Serve() {
 
 func (p *plugin) onServe() {
 	var dummy int
-	if err := p.clnt.Call("RpcHost.ConnectPlugin", p.info, &dummy); err != nil {
+	if err := p.clnt.Call("RPCHost.ConnectPlugin", p.info, &dummy); err != nil {
 		p.log.Fatal(err)
 	}
 }
 
 func (p *plugin) onStop() {
 	var dummy int
-	if err := p.clnt.Call("RpcHost.DisconnectPlugin", p.info, &dummy); err != nil {
+	if err := p.clnt.Call("RPCHost.DisconnectPlugin", p.info, &dummy); err != nil {
 		p.log.Println(err)
 	}
 }
@@ -382,15 +383,14 @@ func (p *plugin) onStop() {
 // NewPlugin creates a new Plugin instance.
 func NewPlugin(pluginName string, serviceName string, options ...*Options) (Plugin, error) {
 	opt := prepareOptions(pluginName, options...)
-	clnt, err := newRpcClient(pluginName, &opt)
+	clnt, err := newRPCClient(pluginName, &opt)
 	if err == nil {
 		opt.Log.Printf("Connected to RPC host: %s\n", opt.dialAddr())
 		info := &PluginInfo{Name: pluginName, ServiceName: serviceName}
-		if err = clnt.Call("RpcHost.GetPort", info, &info.Port); err == nil {
+		if err = clnt.Call("RPCHost.GetPort", info, &info.Port); err == nil {
 			p := &plugin{info: info, log: opt.Log, clnt: clnt}
-			rpcPluginService := &RpcPlugin{plugin: p}
 			srvOpt := prepareOptions(pluginName, &Options{Port: info.Port, Log: opt.Log})
-			if srv, err := newRpcServer(&srvOpt, rpcPluginService); err == nil {
+			if srv, err := newRPCServer(&srvOpt, &RPCPlugin{plugin: p}); err == nil {
 				p.srv = srv
 				srv.onServe(p.onServe)
 				srv.onStop(p.onStop)
@@ -403,13 +403,13 @@ func NewPlugin(pluginName string, serviceName string, options ...*Options) (Plug
 	return nil, err
 }
 
-// Plugin RPC service. Handles  RPC Host requests.
-type RpcPlugin struct {
+// RPCPlugin implements Plugin RPC service. Handles  RPC Host requests.
+type RPCPlugin struct {
 	plugin	*plugin
 }
 
-// Handles RPC Host termination.
-func (rp *RpcPlugin) Terminate(_ int, _ *int) error {
+// Terminate handles RPC Host termination.
+func (rp *RPCPlugin) Terminate(_ int, _ *int) error {
 	rp.plugin.log.Println("Terminating by RPC host request")
 	os.Exit(0)
 	return nil
